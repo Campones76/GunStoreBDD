fb86b7606e430f740352f5ee6dd66ecd14c8d93e
Campones76 <gabrielfernandoferreiraepg@gmail.com>
2023-12-26 16:23:40 +0000

    Revert "tos (n importante)"
    This reverts commit 8e32a13eda89e4ea22c1932087b3cb6d8458e8f2.

--- GunStoreBDD/backend/views/SignupView.py
+++ GunStoreBDD/backend/views/SignupView.py
@@ -1,25 +1,10 @@
-import pypyodbc as odbc
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template
 from .forms import RegistrationForm
 
 signup_bp = Blueprint('SignupView', __name__)
 
-@signup_bp.route('/register', methods=['GET', 'POST'])
+@signup_bp.route('/register', methods=['POST'])
 def register():
+    # Handle signup
     form = RegistrationForm()
-    if form.validate_on_submit():
-        connection_string = 'Driver={SQL Server};Server=school594b;Database=TesteArmas;Trusted_Connection=yes;'
-        conn = odbc.connect(connection_string)
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO dbo.lol (name, email, password) VALUES (?, ?, ?)", (form.username.data, form.email.data, form.password.data))
-        conn.commit()
-        return 'Thanks for registering!'
     return render_template('signup.html', form=form)
-# def register():
-#     # Handle signup
-#     form = RegistrationForm()
-#     if form.is_submitted():
-#         result = request.form
-#         #return render_template('user.html', form=result)
-#         return render_template('signup.html', form=form)
-    
--- GunStoreBDD/backend/views/forms.py
+++ GunStoreBDD/backend/views/forms.py
@@ -5,13 +5,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, valid
 class RegistrationForm(FlaskForm):
     username = StringField('Username', [validators.Length(min=4, max=25)])
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    #password = PasswordField('Password')
-    password = PasswordField('New Password', [
-         validators.DataRequired(),
-         validators.EqualTo('confirm', message='Passwords must match')
-     ])
-    confirm = PasswordField('Repeat Password')
-    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+    password = PasswordField('Password')
     submit = SubmitField('Sign up')
 
 # class RegistrationForm(FlaskForm):
