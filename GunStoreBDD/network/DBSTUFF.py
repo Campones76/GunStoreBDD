789c9f353bcd291d488ff552886f09b0e116e3e9
RDuarte13 <rafael2003duarte@gmail.com>
2023-12-29 19:07:11 +0000

    Primeiro

--- GunStoreBDD/network/DBSTUFF.py
+++ GunStoreBDD/network/DBSTUFF.py
@@ -1,9 +1,10 @@
 # db_config.py
+server='laptop-sgu8bv5l'
 
 DRIVER_NAME = 'ODBC Driver 17 for SQL Server'
-SERVER_NAME = 'SCHOOL594B'
+SERVER_NAME = server.upper()#'SCHOOL594B'  #SCHOOL594B
 DATABASE_NAME = 'GunStore'
-UID = 'GunstoreStaff'
+UID = 'teste' #GunstoreStaff
 PWD = '1234'
 
 connection_string = f"""
